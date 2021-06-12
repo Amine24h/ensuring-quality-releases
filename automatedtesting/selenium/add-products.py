c27e567bc89be254223ae5b940c84abcc631f5e2
Amine KABOUCHE <amine.caius@gmail.com>
2021-06-12 14:41:56 +0100

    Add Jmeter tests

--- automatedtesting/selenium/add-products.py
+++ automatedtesting/selenium/add-products.py
@@ -12,7 +12,7 @@ def login (user, password):
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     
-    print (f'Trying to login a user {user}')
+    print ('Trying to login a user {}'.format(user))
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
     driver.find_element_by_css_selector("input[id='login-button']").click()
@@ -24,15 +24,22 @@ def login (user, password):
         print('ERROR! when login you in!')
 
 def add_products ():
-    print ('Adding all products to cart...')
+    title_products = driver.find_elements_by_css_selector("div[class='inventory_item_name']")
+    for title in title_products:
+        print ('Adding product {}'.format(title.text))
     buttons_add_to_cart = driver.find_elements_by_css_selector("button[class='btn btn_primary btn_small btn_inventory']")
     for button in buttons_add_to_cart:
         button.click()
-    
-    print ('Removing all products from cart...')
+
+    print ('Successfully added all products')
+
+    for title in title_products:
+        print ('Removing product {}'.format(title.text))
     buttons_remove_from_cart = driver.find_elements_by_css_selector("button[class='btn btn_secondary btn_small btn_inventory']")
     for button in buttons_remove_from_cart:
         button.click()
+
+    print ('Successfully removed all products')
 
 
 login('standard_user', 'secret_sauce')
